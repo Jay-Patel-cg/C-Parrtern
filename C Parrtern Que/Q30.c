a968ca49f7f80b23383e26396fec0f7c838cb322
Jay-Patel-cg <jay.patel.a.cg@gmail.com>
2025-09-25 12:26:34 +0530

    Change

--- C Parrtern Que/Q30.c
+++ C Parrtern Que/Q30.c
@@ -1,4 +1,47 @@
+// #include <stdio.h>
+// int main()
+// {
+
+//     int n;
+//     printf("Enter The Value : ");
+//     scanf("%d",&n);
+
+//     for(int i=1; i<=n; i++)
+//     {
+//         for(int j=1; j<=n-i; j++)
+//         {
+//             printf(" ");
+//         }
+//         for(int k=1; k<= (2 * i - 1); k++)
+//         {
+//             printf("%c",k+64);
+//         }
+//         printf("\n");
+//     }
+
+//     //  int n;
+//     // printf("Enter The Value : ");
+//     // scanf("%d",&n);
+
+//     for(int l=1; l<=n; l++)
+//     {
+//         for(int m=1; m<=l; m++)
+//         {
+//             printf(" ");
+//         }
+//         for(int o=1; o<=2*(n-l)-1; o++)
+//         {
+//             printf("%c",o+64);
+//         }
+//         printf("\n");
+//     }
+
+
+//     return 0;
+// }
+
 #include <stdio.h>
+
 int main()
 {
 
@@ -6,35 +49,8 @@ int main()
     printf("Enter The Value : ");
     scanf("%d",&n);
 
-    for(int i=1; i<=n; i++)
-    {
-        for(int j=1; j<=n-i; j++)
-        {
-            printf(" ");
-        }
-        for(int k=1; k<= (2 * i - 1); k++)
-        {
-            printf("%c",k+64);
-        }
-        printf("\n");
-    }
+    for(int i=0; )
 
-    //  int n;
-    // printf("Enter The Value : ");
-    // scanf("%d",&n);
-
-    for(int l=1; l<=n; l++)
-    {
-        for(int m=1; m<=l; m++)
-        {
-            printf(" ");
-        }
-        for(int o=1; o<=2*(n-l)-1; o++)
-        {
-            printf("%c",o+64);
-        }
-        printf("\n");
-    }
 
 
     return 0;
--- C Parrtern Que/Q34.c
+++ C Parrtern Que/Q34.c
@@ -1,50 +1,50 @@
-// #include <stdio.h>
-
-// int main()
-// {
-//     int n;
-//     printf("Enter The Value : ");
-//     scanf("%d",&n);
-
-//     for(int i=n; i>0;i--)
-//     {
-//         for(int j=i;j<=n; j++)
-//         {
-//             printf(" ");
-//         }
-//         for(int k=n; k>=i; k--)
-//         {
-//             printf("*");
-//         }
-//         printf("\n");
-//     }
-
-
-//     return 0;
-// }
-
-
-
-
-
-
-
-
 #include <stdio.h>
 
 int main()
 {
-
     int n;
-    printf("ENter The Value : ");
+    printf("Enter The Value : ");
     scanf("%d",&n);
 
-    for(int i=1; i)
-
+    for(int i=n; i>0;i--)
+    {
+        for(int j=i; j<=n; j++)
+        {
+            printf(" ");
+        }
+        for(int k=n; k>=i; k--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
 
 
     return 0;
 }
+
+
+
+
+
+
+
+
+// #include <stdio.h>
+
+// int main()
+// {
+
+//     int n;
+//     printf("ENter The Value : ");
+//     scanf("%d",&n);
+
+//     for(int i=1; i)
+
+
+
+//     return 0;
+// }
 
 
 
